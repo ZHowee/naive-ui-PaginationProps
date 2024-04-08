2c78445e14db706649158e096daf27b21a0961f9
Skall <skall.wu@cyanstone.net>
2024-04-08 17:48:17 +0800

    init

---
+++ src/test.vue
@@ -0,0 +1,14 @@
+<template>
+  <NButton v-bind="buttonProps">button</NButton>
+  <NPagination v-bind="pagination" />
+  <NDataTable :pagination="pagination" />
+</template>
+
+<script lang="ts" setup>
+import { reactive } from "vue";
+import { NButton, NPagination, NDataTable } from "naive-ui";
+import type { PaginationProps, ButtonProps } from "naive-ui";
+
+const buttonProps = reactive<ButtonProps>({});
+const pagination = reactive<PaginationProps>({});
+</script>
